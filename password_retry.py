8905a28cdd59098a0d22be9dac7bc2b90691a123
王弈 <wangyi@wangyide-MacBook-Air.local>
2021-07-13 20:57:32 +0800

    trys > 0 & account locked down

--- password_retry.py
+++ password_retry.py
@@ -1,11 +1,15 @@
 # 密碼重設程式
 password = 'a123456' 
-trys = 3 # trys = 嘗試次數
+trys = 3 # trys = 剩餘機會
 while trys > 0:
+	trys = trys - 1
 	pwd = input('請輸入您的密碼： ') 
 	if pwd == password:
 		print('登入成功！')
 		break
 	else:
-		trys = trys - 1
-		print('密碼輸入錯誤！還有', trys, '次機會')
+		print('密碼輸入錯誤！')
+		if trys > 0:
+			print('還有', trys, '次機會')
+		else:
+			print('沒有機會了，要鎖帳號了拉！')
